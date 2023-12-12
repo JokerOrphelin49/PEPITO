22a933fa29c5528268cda9058692eebb35ed4fea
Siklair <98527881+Siklair@users.noreply.github.com>
2023-12-12 11:32:26 +0100

    correction indent

--- main.py
+++ main.py
@@ -14,16 +14,16 @@ GPIO.setup(SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 x = 0
 
 while True:
-	oldIsOpen = isOpen 
-    isOpen = GPIO.input(DOOR_SENSOR_PIN) 
+	oldIsOpen = isOpen
+	isOpen = GPIO.input(DOOR_SENSOR_PIN)
 
-    if (isOpen and (isOpen != oldIsOpen)):  
-        print "Space is unoccupied!"  
-        GPIO.output(RED, False)  
-        GPIO.output(YELLOW, True) 
-    elif (isOpen != oldIsOpen):  
-        print "Space is occupied!"  
-        GPIO.output(YELLOW, False)  
-        GPIO.output(RED, True)  
+    if (isOpen and (isOpen != oldIsOpen)):
+        print "Space is unoccupied!"
+        GPIO.output(RED, False)
+        GPIO.output(YELLOW, True)
+    elif (isOpen != oldIsOpen):
+        print "Space is occupied!"
+        GPIO.output(YELLOW, False)
+        GPIO.output(RED, True)
 
     time.sleep(0.1)
