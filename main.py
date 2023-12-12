36d7733bdaa1f88d6b206dc2c602872256369d9a
Siklair <98527881+Siklair@users.noreply.github.com>
2023-12-12 11:30:12 +0100

    Test avec des leds

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(40, GPIO.IN)
-GPIO.setup(12, GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+
+RED = 14
+YELLOW = 15
+SENSOR = 18
+
+GPIO.setup(RED, GPIO.OUT)
+GPIO.setup(YELLOW, GPIO.OUT)
+GPIO.setup(SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 x = 0
 
 while True:
-	if GPIO.input(40) == GPIO.HIGH:
-		print(f"abababa{x}")
-		x += 1
+	oldIsOpen = isOpen 
+    isOpen = GPIO.input(DOOR_SENSOR_PIN) 
+
+    if (isOpen and (isOpen != oldIsOpen)):  
+        print "Space is unoccupied!"  
+        GPIO.output(RED, False)  
+        GPIO.output(YELLOW, True) 
+    elif (isOpen != oldIsOpen):  
+        print "Space is occupied!"  
+        GPIO.output(YELLOW, False)  
+        GPIO.output(RED, True)  
+
+    time.sleep(0.1)
