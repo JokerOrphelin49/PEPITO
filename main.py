5a403d9c87652ede4f077396db6213a2be52e531
Siklair <98527881+Siklair@users.noreply.github.com>
2023-12-18 18:57:53 +0100

    Comments

--- main.py
+++ main.py
@@ -4,26 +4,36 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
+# Pin numbers
 RED = 14
 YELLOW = 15
 SENSOR = 18
 
+# Set the modes of each pin
+# RED and YELLOW are LEDs for testing
 GPIO.setup(RED, GPIO.OUT)
 GPIO.setup(YELLOW, GPIO.OUT)
 GPIO.setup(SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 x = 0
 
+# Variables to check if the state changes
 isOpen = None
 oldIsOpen = None
 
+# Variables to wait some time before acting (ex: if the door rapidly opens and closes in repetition)
+# May not be a good idea if used as a security device
 TIME_BEFORE_ACTION = 5 # seconds
 time_from_change = 0
 
 while True:
+	# Get the state of the sensor
 	isOpen = GPIO.input(SENSOR)
 
+	# the door opened
 	if (isOpen and (isOpen != oldIsOpen)):
+		# Wait some time
+		# Need to be fixed, the timer should reset when the state of the sensor changes
 		if time_from_change >= TIME_BEFORE_ACTION:
 			print("Opened!")
 			GPIO.output(RED, False)
@@ -31,8 +41,9 @@ while True:
 			time_from_change = 0
 			oldIsOpen = isOpen
 		else:
+			# should be improved
 			time_from_change += 0.1
-	elif (isOpen != oldIsOpen):
+	elif (isOpen != oldIsOpen): # the door closed
 		if time_from_change >= TIME_BEFORE_ACTION:
 			print("Closed!")
 			GPIO.output(YELLOW, False)
