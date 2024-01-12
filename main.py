2163b5a785162961738ddc72a4cf5c2840895125
Siklair <98527881+Siklair@users.noreply.github.com>
2024-01-12 14:18:03 +0100

    better code

--- main.py
+++ main.py
@@ -14,32 +14,51 @@ GPIO.setup(SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 x = 0
 
-isOpen = None
-oldIsOpen = None
+isOpen = None # sensor 
+oldIsOpen = None 
 
 TIME_BEFORE_ACTION = 5 # seconds
 time_from_change = 0
 
+def onOpen():
+	print("Opened!")
+	GPIO.output(RED, False)
+	GPIO.output(YELLOW, True)
+
+def onClose():
+	print("Closed!")
+	GPIO.output(YELLOW, False)
+	GPIO.output(RED, True)
+
 while True:
 	isOpen = GPIO.input(SENSOR)
 
-	if (isOpen and (isOpen != oldIsOpen)):
+	if (isOpen != oldIsOpen):
 		if time_from_change >= TIME_BEFORE_ACTION:
-			print("Opened!")
-			GPIO.output(RED, False)
-			GPIO.output(YELLOW, True)
 			time_from_change = 0
 			oldIsOpen = isOpen
+			if (isOpen):
+				onOpen()
+			else:
+				onClose()
 		else:
+			time_from_change += 0.1
+	
+
+
+	""" if (isOpen and (isOpen != oldIsOpen)):
+		if time_from_change >= TIME_BEFORE_ACTION:
+			onOpen()
+			time_from_change = 0
+			oldIsOpen = isOpen
+		else: # à améliorer
 			time_from_change += 0.1
 	elif (isOpen != oldIsOpen):
 		if time_from_change >= TIME_BEFORE_ACTION:
-			print("Closed!")
-			GPIO.output(YELLOW, False)
-			GPIO.output(RED, True)
+			onClose()
 			time_from_change = 0
 			oldIsOpen = isOpen
 		else:
-			time_from_change += 0.1
+			time_from_change += 0.1 """
 
 	time.sleep(0.1)
