8349e52de86a167b644653e2d4efd02fb0ae3278
Siklair <hamoir.alex@gmail.com>
2024-05-12 15:31:34 +0200

    Fix la logique + update webhook link pour test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord import Webhook
 import aiohttp
 import asyncio
 from time import sleep
+
 #Buzzer
 BUZZER = 4
 
@@ -11,11 +12,9 @@ BUZZER = 4
 def send_message(msg: str):
 	async def inner():
 	    async with aiohttp.ClientSession() as session:
-	        webhook = Webhook.from_url('https://discord.com/api/webhooks/1195361569270407259/VdWl_9swoRMxeUnEJYuwHeyKHXWXs0nYIc4ReFvBFg6itygQxzmUty2idMwXkDlpmLlq', session=session)
+	        webhook = Webhook.from_url('https://discordapp.com/api/webhooks/1239175726641451008/U8uIUUcKhWd2FnEVxfJoPlQ-Q2YTz825B0GSDgtvjUirOmG-eXW8XH8CUJaAm8WG9UFk', session=session)
 	        await webhook.send(msg, username='Pepito Ier')
 	asyncio.run(inner())
-
- 
 
 # variables pour le buzzer
 GPIO.setwarnings(False)
@@ -70,35 +69,14 @@ while True:
         # on vérifie si le délai est dépassé
 		if time_from_change >= TIME_BEFORE_ACTION:
 			time_from_change = 0
-			oldIsOpen = isOpen
 			if (isOpen):
 				onOpen()
 			else:
 				onClose()
-            # musique jouée par le buzzer à chaque ouverture/fermeture
-			GPIO.output(BUZZER, GPIO.HIGH)
-			pin7.ChangeFrequency(16.35) # C0
-			sleep(1)
-			pin7.ChangeFrequency(261.63) # C4
-			sleep(1)
-			pin7.ChangeFrequency(293.66) # D4
-			sleep(1)
-			pin7.ChangeFrequency(329.63) # E4
-			sleep(1)
-			pin7.ChangeFrequency(349.23) # F4
-			sleep(1)
-			pin7.ChangeFrequency(392.00) # G4
-			sleep(1)
-			pin7.ChangeFrequency(440.00) # A4
-			sleep(1)
-			pin7.ChangeFrequency(493.88) # B4
-			sleep(1)
-			pin7.ChangeFrequency(523.25) # A5
-			sleep(1.5)
-			pin7.ChangeFrequency(16.35) # C0
-			sleep(1)
-			GPIO.output(BUZZER, GPIO.LOW)
+			oldIsOpen = isOpen
 		else:
 			time_from_change += 0.1
+	else:
+		time_from_change = 0
 
 	time.sleep(0.1)
