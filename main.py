43fffed41d6378d0977c0a5ad57a0df4e66fc696
Siklair <hamoir.alex@gmail.com>
2024-05-12 15:37:21 +0200

    clean up code

--- main.py
+++ main.py
@@ -5,23 +5,8 @@ import aiohttp
 import asyncio
 from time import sleep
 
-#Buzzer
-BUZZER = 4
-
-# Envoyer un message sur discord
-def send_message(msg: str):
-	async def inner():
-	    async with aiohttp.ClientSession() as session:
-	        webhook = Webhook.from_url('https://discordapp.com/api/webhooks/1239175726641451008/U8uIUUcKhWd2FnEVxfJoPlQ-Q2YTz825B0GSDgtvjUirOmG-eXW8XH8CUJaAm8WG9UFk', session=session)
-	        await webhook.send(msg, username='Pepito Ier')
-	asyncio.run(inner())
-
-# variables pour le buzzer
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(BUZZER, GPIO.OUT)
-pin7 = GPIO.PWM(BUZZER, 100)
-pin7.start(50)
+# time step
+STEP = 0.1
 
 # Pin numbers
 RED = 14
@@ -34,30 +19,39 @@ GPIO.setup(RED, GPIO.OUT)
 GPIO.setup(YELLOW, GPIO.OUT)
 GPIO.setup(SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
-x = 0
-
 # Variables to check if the state changes
-isOpen = None # sensor 
-oldIsOpen = None 
+isOpen = None # Current door state
+oldIsOpen = None # Last registered door state (for which an action was taken)
 
 # Variables to wait some time before acting (ex: if the door rapidly opens and closes in repetition)
-# May not be a good idea if used as a security device
-TIME_BEFORE_ACTION = 5 # seconds
+TIME_BEFORE_ACTION = 5 # delay in seconds
 time_from_change = 0
+
+# Messages
+OPENED_MESSAGE = "Porte Ouverte"
+CLOSED_MESSAGE = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR01SR_zVtraxBzNdicrhSqSBOxZShSeMlHr2W5WcEbaA&s"
 
 # action à réaliser lors de l'ouverture
 def onOpen():
-	send_message("Porte Ouverte")
+	send_message(OPENED_MESSAGE)
 	print("Opened!")
 	GPIO.output(RED, False)
 	GPIO.output(YELLOW, True)
 
 # action à réaliser lors de la fermeture
 def onClose():
-	send_message("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR01SR_zVtraxBzNdicrhSqSBOxZShSeMlHr2W5WcEbaA&s")
+	send_message(CLOSED_MESSAGE)
 	print("Closed!")
 	GPIO.output(YELLOW, False)
 	GPIO.output(RED, True)
+
+# Envoyer un message sur discord
+def send_message(msg: str):
+	async def inner():
+	    async with aiohttp.ClientSession() as session:
+	        webhook = Webhook.from_url('https://discordapp.com/api/webhooks/1239175726641451008/U8uIUUcKhWd2FnEVxfJoPlQ-Q2YTz825B0GSDgtvjUirOmG-eXW8XH8CUJaAm8WG9UFk', session=session)
+	        await webhook.send(msg, username='Pepito Ier')
+	asyncio.run(inner())
 
 # boucle principale
 while True:
@@ -75,8 +69,8 @@ while True:
 				onClose()
 			oldIsOpen = isOpen
 		else:
-			time_from_change += 0.1
+			time_from_change += STEP
 	else:
 		time_from_change = 0
 
-	time.sleep(0.1)
+	time.sleep(STEP)
